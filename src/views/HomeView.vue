d0a6b0bbb17d5ddae9d292a72af51639007a878d
kelvinsekx <utdkelvin@gmail.com>
2025-03-03 04:00:26 +0100

    teleport modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -98,106 +98,108 @@ const tabs = ['stocks', 'watchlists']
       </CxCard>
     </div>
   </main>
-  <Modal :showing="showModal" @toggle-modal="showModal = false">
-    <div class="w-full space-y-5">
-      <div class="space-y-2">
-        <div class="flex flex-col">
-          <span class="text-3xl font-bold tracking-tight">AAPL</span>
-          <span class="text-[#222]">Apple Inc.</span>
+  <teleport to="#modal-point">
+    <Modal :showing="showModal" @toggle-modal="showModal = false">
+      <div class="w-full space-y-5">
+        <div class="space-y-2">
+          <div class="flex flex-col">
+            <span class="text-3xl font-bold tracking-tight">AAPL</span>
+            <span class="text-[#222]">Apple Inc.</span>
+          </div>
+          <button
+            class="border border-[#555]/60 rounded-md px-3 font-normal text-base text-[#555] flex py-2"
+          >
+            <IconPlus class="text-[#555] stroke-1" /> Add to Watchlist
+          </button>
         </div>
-        <button
-          class="border border-[#555]/60 rounded-md px-3 font-normal text-base text-[#555] flex py-2"
-        >
-          <IconPlus class="text-[#555] stroke-1" /> Add to Watchlist
-        </button>
-      </div>
-      <div class="flex gap-4 text-[#222] font-semibold">
-        <CxCard>
-          <p class="text-xl font-semibold tracking-tight">Pricing Information</p>
-          <div class="flex justify-between items-center">
-            <div>
-              <span class="text-5xl tracking-tighter font-semibold">$233</span>
-              <div class="text-green-400">
-                <span>344 </span>
-                <span>( 344% )</span>
+        <div class="flex gap-4 text-[#222] font-semibold">
+          <CxCard>
+            <p class="text-xl font-semibold tracking-tight">Pricing Information</p>
+            <div class="flex justify-between items-center">
+              <div>
+                <span class="text-5xl tracking-tighter font-semibold">$233</span>
+                <div class="text-green-400">
+                  <span>344 </span>
+                  <span>( 344% )</span>
+                </div>
+              </div>
+              <div class="flex flex-col">
+                <span class="text-[#999] font-normal tracking-tight">Market Cap</span>
+                <span>$2333 B</span>
               </div>
             </div>
-            <div class="flex flex-col">
-              <span class="text-[#999] font-normal tracking-tight">Market Cap</span>
-              <span>$2333 B</span>
+            <div class="grid grid-cols-2 gap-y-2 font-semibold">
+              <div class="w-[10rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Open</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Previous close</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Day's Range</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">52 week range</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="grid grid-cols-2 gap-y-2 font-semibold">
-            <div class="w-[10rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Open</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
+          </CxCard>
+          <CxCard>
+            <p class="text-xl font-semibold tracking-tight">Information Information</p>
+            <div class="flex justify-between items-center">
+              <div class="flex flex-col">
+                <span class="text-[#999] font-normal tracking-tight">Volume</span>
+                <span>$2333344</span>
+              </div>
+              <div class="flex flex-col">
+                <span class="text-[#999] font-normal tracking-tight">Av. Volume</span>
+                <span>$2333 B</span>
+              </div>
             </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Previous close</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
+            <div class="grid grid-cols-2 gap-y-2 font-semibold">
+              <div class="w-[10rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Open</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Previous close</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">Day's Range</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
+              <div class="w-[20rem]">
+                <p class="flex flex-col">
+                  <span class="text-[#999] font-normal tracking-tight">52 week range</span>
+                  <span class="text-[#222]">186.5</span>
+                </p>
+              </div>
             </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Day's Range</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">52 week range</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-          </div>
-        </CxCard>
+          </CxCard>
+        </div>
         <CxCard>
-          <p class="text-xl font-semibold tracking-tight">Information Information</p>
-          <div class="flex justify-between items-center">
-            <div class="flex flex-col">
-              <span class="text-[#999] font-normal tracking-tight">Volume</span>
-              <span>$2333344</span>
-            </div>
-            <div class="flex flex-col">
-              <span class="text-[#999] font-normal tracking-tight">Av. Volume</span>
-              <span>$2333 B</span>
-            </div>
-          </div>
-          <div class="grid grid-cols-2 gap-y-2 font-semibold">
-            <div class="w-[10rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Open</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Previous close</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">Day's Range</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-            <div class="w-[20rem]">
-              <p class="flex flex-col">
-                <span class="text-[#999] font-normal tracking-tight">52 week range</span>
-                <span class="text-[#222]">186.5</span>
-              </p>
-            </div>
-          </div>
+          <p>Price Trend</p>
+          <div class="h-[10rem]"></div>
         </CxCard>
       </div>
-      <CxCard>
-        <p>Price Trend</p>
-        <div class="h-[10rem]"></div>
-      </CxCard>
-    </div>
-  </Modal>
+    </Modal>
+  </teleport>
 </template>
