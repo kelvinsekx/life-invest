c526c65565396438357183dd95d60f75ef5c37ab
kelvinsekx <utdkelvin@gmail.com>
2025-03-04 11:35:36 +0100

    resolving ctx type

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,14 +47,9 @@ const chartData = {
 }
 
 const chartjsINIT = () => {
-  let ctx = null
-  if (chartRef.value) {
-    const canvas = chartRef.value || document.getElementById('chart-canvas')
+  const ctx = chartRef.value || document.getElementById('chart-canvas')
 
-    ctx = canvas
-  }
-
-  for (let i = 0; i < ctx.length; i++) {
+  for (let i = 0; i < ctx?.length; i++) {
     new Chart(ctx[i].getContext('2d'), {
       type: 'line',
       data: chartData,
